0a8f82c8dbff1610ab7ef3e443de2fd448de295d
Candra Marbun <anakgadong@gmail.com>
2020-05-11 21:00:27 +0700

    fressh install

---
+++ src/test/java/com/marbun/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marbun.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
